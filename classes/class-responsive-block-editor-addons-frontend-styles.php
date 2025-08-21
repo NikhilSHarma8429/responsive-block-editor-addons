f1200b143f79965b4dce900a3b39384095fbe400
Nikhil Sharma <nikhil.sharma@hbwsl.com>
2025-08-21 09:47:41 +0530

    fixed default right padding

--- classes/class-responsive-block-editor-addons-frontend-styles.php
+++ classes/class-responsive-block-editor-addons-frontend-styles.php
@@ -6857,6 +6857,7 @@ if ( ! class_exists( 'Responsive_Block_Editor_Addons_Frontend_Styles' ) ) {
 				'customWidth'              => '',
 				'customHeight'             => '',
 				'columnsize'               => 3,
+				'filterTabRightPaddingMobile' => 30,
 			);
 		}
 
